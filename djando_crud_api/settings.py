65c8843e15eab7509effb08e1534472f1ebdbdac
cristian <cristiangaetejordan@gmail.com>
2023-07-13 20:09:03 -0400

    nuevos cambios

--- djando_crud_api/settings.py
+++ djando_crud_api/settings.py
@@ -128,8 +128,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #cors authorization
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-   
+   "http://localhost:5173",
 ]
 
 REST_FRAMEWORK = {
